a060c1d6ba13fc327e37db38827ed722906f777e
2021dbakhmat <ubuntu@ide.ide.e055c8f4-0a78-4ff9-9372-bd7d6ee7a4ec.svc.cluster.local>
2019-09-17 13:16:20 +0000

    first commit

---
+++ greeting.py
@@ -0,0 +1,12 @@
+name1 = "Diana"
+name2 = "Marl owe"
+name3 = "sister"
+
+print(name1)
+print(name2)
+print("hello, " + name1)
+print(len(name2))
+print(name2 + " has " + str(len(name2)) + " characters in their name!")
+
+age1 = "Killian"
+print("My" (name3 + " is " + str(len(age1)) + " years old!" )
